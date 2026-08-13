52ebceeac9582ffbcf116400c414ca660b50e7eb
amatda-team <amatda.team@gmail.com>
2026-08-13 22:56:14 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+const db = [
+  {
+    id: 1,
+    word: "観衆",
+    reading: "かんしゅう",
+    meaning: "관중",
+    tag: "テスト",
+    exJa: "<ruby>観衆<rt>かんしゅう</rt></ruby>が<ruby>危険<rt>きけん</rt></ruby>に<ruby>晒<rt>さら</rt></ruby>される<ruby>恐<rt>おそ</rt></ruby>れがある。",
+    exKo: "관중이 위험에 처할 우려가 있다."
+  },
+  {
+    id: 2,
+    word: "好ましい",
+    reading: "このましい",
+    meaning: "바람직하다, 마음에 들다",
+    tag: "日常",
+    exJa: "<ruby>面接<rt>めんせつ</rt></ruby>では、<ruby>清潔感<rt>せいけつかん</rt></ruby>のある<ruby>服装<rt>ふくそう</rt></ruby>が好ましい。",
+    exKo: "면접에서는 청결감이 있는 복장이 바람직하다."
+  },
+  {
+    id: 3,
+    word: "無防備",
+    reading: "むぼうび",
+    meaning: "무방비",
+    tag: "日常",
+    exJa: "SNSに<ruby>個人情報<rt>こじんじょうほう</rt></ruby>を<ruby>載<rt>の</rt></ruby>せるのは無防備だと<ruby>言<rt>い</rt></ruby>わざるを<ruby>得<rt>え</rt></ruby>ない。",
+    exKo: "SNS에 개인정보를 올리는 것은 무방비하다고 할 수밖에 없다."
+  }
+];
